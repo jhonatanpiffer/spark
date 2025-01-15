3be56f39790d6ca879fe3894362c68747a738835
Jhonatan Piffer Siqueira <jhonatan.p.s@gmail.com>
2025-01-15 12:10:23 -0300

    First Map example

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -12,21 +12,29 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Double> inputData = new ArrayList<>();
-		inputData.add(35.5);
-		inputData.add(12.49943);
-		inputData.add(90.32);
-		inputData.add(20.32);
-		SparkConf conf = new SparkConf().setAppName("StartingSpark").setMaster("local[*]");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		List<Integer> inputData = new ArrayList<>();
+		inputData.add(35);
+		inputData.add(12);
+		inputData.add(90);
+		inputData.add(20);
 		
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		final Logger logger = Logger.getLogger(Main.class);
 		
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("StartingSpark");
 		
-		JavaRDD<Double> myRDD = sc.parallelize(inputData);
+		JavaSparkContext sc = new JavaSparkContext(conf);
+				
+		JavaRDD<Integer> myRDD = sc.parallelize(inputData);
 		
-		Double result = myRDD.reduce( (value1, value2) -> value1 + value2);
-		System.out.println(resultd);
+		Integer result = myRDD.reduce( (value1, value2) -> value1 + value2);
+		
+		JavaRDD<Double> sqrtRdd = myRDD.map(value -> Math.sqrt(value));
+		
+		sqrtRdd.foreach(value -> System.out.println(value));
+		
+		System.out.println(result);
 		
 		sc.close();
 		
