013ddc99d78ac3eeba1fc8e124e4eafa3ed5a69e
Jhonatan Piffer Siqueira <jhonatan.p.s@gmail.com>
2025-01-15 18:49:39 -0300

    Loading a text file on a RDD

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -14,20 +14,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> inputData = new ArrayList<>();
-		inputData.add("WARN: Tuesday 4 September 0405");
-		inputData.add("ERROR: Tuesday 4 September 0408");
-		inputData.add("FATAL: Wednesday 5 September 1632");
-		inputData.add("ERROR: Friday 7 September 1854");
-		inputData.add("WARN: Saturday 8 September 1942");
-		
-		Logger.getLogger("org.apache").setLevel(Level.WARN);
-		
+		Logger.getLogger("org.apache").setLevel(Level.WARN);		
 		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("StartingSpark");
-		
 		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
 				
-		sc.parallelize(inputData)
+		initialRdd
 			.flatMap(value -> Arrays.asList(value.split(" ")).iterator())
 			.filter(word -> word.length() > 1  )
 			.foreach(value -> System.out.println(value));
