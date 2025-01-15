755c10ebabbb0625deb77635540c96e4306818f1
Jhonatan Piffer Siqueira <jhonatan.p.s@gmail.com>
2025-01-15 13:44:44 -0300

    Simples split string creating a PairRDD

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -6,18 +6,22 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		List<Integer> inputData = new ArrayList<>();
-		inputData.add(35);
-		inputData.add(12);
-		inputData.add(90);
-		inputData.add(20);
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN: Tuesday 4 September 0405");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Friday 7 September 1854");
+		inputData.add("WARN: Saturday 8 September 1942");
 		
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		
@@ -25,12 +29,15 @@ public class Main {
 		
 		JavaSparkContext sc = new JavaSparkContext(conf);
 				
-		JavaRDD<Integer> originalIntergers = sc.parallelize(inputData);
-		JavaRDD<IntegetWithSquareRoot> sqrtRdd = originalIntergers.map(value -> new IntegetWithSquareRoot(value));
-		
-		sqrtRdd.foreach((IntegetWithSquareRoot n) -> System.out.printf("%d %f\n",n.getOrignalNumber(), n.getSqrt()));
-		
-		
+		JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+		JavaPairRDD<String, String>  pairRdd = originalLogMessages.mapToPair(rawvalue -> {
+			String[] columns = rawvalue.split(":");
+			String level = columns[0];
+			String date = columns[1];
+			return new Tuple2<>(level, date);
+		});
+
+		System.out.println(pairRdd.count());
 		sc.close();
 	}
 
